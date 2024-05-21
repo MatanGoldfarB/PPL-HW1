59a9db6a9d7b960513f1c88a9cde28963180d936
Matan Goldfarb <matangoldfarb1@gmail.com>
2024-05-21 15:02:46 +0300

    question 2

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,12 +1,31 @@
 import * as R from "ramda";
 //AAAAAAAAAAAAAA
 const stringToArray = R.split("");
-//MMMMMMMMMM
+
 /* Question 1 */
-export const countVowels: undefined = undefined
+
+export const countVowels: (str:string) => number = R.pipe(
+    stringToArray,
+    R.reduce((acc: number, cur: string) => "aeiouAEIOU".includes(cur) ? acc+1 : acc, 0),
+);
 
 /* Question 2 */
-export const isPaired: undefined = undefined
+const popPair = (stack: string[], open: string, close: string): string[] =>
+    R.isEmpty(stack) ? [close] : 
+    R.head(stack)===open ? R.tail(stack) : R.prepend(close, stack);
+
+const pairCheck = (stack: string[], ch: string): string[] => 
+    (ch === "(" || ch === "{"|| ch === "[") ? R.prepend(ch, stack) : 
+    ch === ")" ? popPair(stack, "(", ch) :
+    ch === "}" ? popPair(stack, "{", ch) :
+    ch === "]" ? popPair(stack, "[", ch) :
+    stack;
+
+export const isPaired: (str:string) => boolean = R.pipe(
+    stringToArray,
+    R.reduce(pairCheck,[]),
+    R.isEmpty
+);
 
 /* Question 3 */
 export type WordTree = {
@@ -14,5 +33,6 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence : undefined = undefined
+export const treeToSentence : (tree: WordTree) => string = (tree) => 
+    tree.root +tree.children.reduce((acc: string,cur: WordTree) => acc + treeToSentence(cur)," ");
 
